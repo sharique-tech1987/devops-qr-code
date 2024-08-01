bc2dbae06a99b3586c2f4411b0ea1a225881f617
skhan1987 <skhan@get-licensed.co.uk>
2024-08-01 10:18:37 +0200

    added api router
    added api router to call backend service with /api route

---
+++ api/app/api_router.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter
+import os
+import qrcode
+import boto3
+import os
+from io import BytesIO
+
+
+router = APIRouter(prefix='/api')
+
+@router.get("/test/")
+def read_root():
+    return {'Hello': os.getenv("BASE_URL") + ":" + os.getenv("PORT")}
+
+# AWS S3 Configuration
+s3 = boto3.client(
+    's3',
+    aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
+
+bucket_name = 'sharique-devops-qr-code' # Add your bucket name here
+
+@router.post("/generate-qr/")
+async def generate_qr(url: str):
+    # Generate QR Code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    
+    # Save QR Code to BytesIO object
+    img_byte_arr = BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+
+    # Generate file name for S3
+    file_name = f"qr_codes/{url.split('//')[-1]}.png"
+
+    try:
+        # Upload to S3
+        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
+        
+        # Generate the S3 URL
+        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+        return {"qr_code_url": s3_url}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
--- api/app/main.py
+++ api/app/main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import qrcode
-import boto3
 import os
-from io import BytesIO
+from . import api_router
 
 # Loading Environment variable (AWS Access Key and Secret Key)
 from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI()
+# Mount the API router to the main app
+app.include_router(api_router.router)
 
 # Allowing CORS for local testing
 origins = [
-    "http://k8s.sharique.cloud:3000"
+    os.getenv("BASE_URL") + ":" + os.getenv("PORT") 
 ]
 
 app.add_middleware(
@@ -23,47 +23,3 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# AWS S3 Configuration
-s3 = boto3.client(
-    's3',
-    aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
-
-bucket_name = 'sharique-devops-qr-code' # Add your bucket name here
-
-@app.get("/api/test/")
-def read_root():
-    return {'Hello': 'World'}
-
-@app.post("/api/generate-qr/")
-async def generate_qr(url: str):
-    # Generate QR Code
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
-
-    img = qr.make_image(fill_color="black", back_color="white")
-    
-    # Save QR Code to BytesIO object
-    img_byte_arr = BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
-
-    # Generate file name for S3
-    file_name = f"qr_codes/{url.split('//')[-1]}.png"
-
-    try:
-        # Upload to S3
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
-        
-        # Generate the S3 URL
-        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
-        return {"qr_code_url": s3_url}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
